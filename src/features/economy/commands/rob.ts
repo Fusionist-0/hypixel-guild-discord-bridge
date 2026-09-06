3d86da92d2287b8d51598a0dbe3f50894fd55346
Fusionist-0 <fusionplayz0000@gmail.com>
2026-09-05 21:47:23 -0300

    fix(cmds): $rob erroring when amount is 0

--- src/features/economy/commands/rob.ts
+++ src/features/economy/commands/rob.ts
@@ -34,6 +34,11 @@ export default class Rob extends ChatCommandHandler {
     }
 
     const amount = resolveAmount(context, context.args.at(1))
+    if (Number(amount) <= 0) {
+      context.resetCooldown()
+      return `${context.message.user.displayName()}, amount must be at least 1!`
+    }
+
     if (typeof amount === 'string') {
       context.resetCooldown()
       return amount
